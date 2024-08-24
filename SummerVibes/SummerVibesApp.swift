f40ccb869e78180214de6459721ecd978d4e9e05
gromozekapp <gromozeka@abv.bg>
2024-08-24 13:14:31 +0300

    Initial Commit

---
+++ SummerVibes/SummerVibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerVibesApp.swift
+//  SummerVibes
+//
+//  Created by Daniil Zolotarev on 24.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SummerVibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
