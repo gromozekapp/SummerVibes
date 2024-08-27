1d83302ac7632771b18610a90895e5405610744e
gromozekapp <gromozeka@abv.bg>
2024-08-27 16:50:59 +0300

    create README and License

---
+++ SummerVibes/PostModel.swift
@@ -0,0 +1,16 @@
+//
+//  PostModel.swift
+//  SummerVibes
+//
+//  Created by Daniil Zolotarev on 24.08.24.
+//
+
+import UIKit
+
+struct PostModel {
+    var image: UIImage
+    var title: String
+    var text: String
+    var author: String
+    var date: String
+}
